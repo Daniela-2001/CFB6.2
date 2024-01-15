1fe954e479a54fa49ac32077b9625cd23a782387
Daniela Duarte <danielatavaresd@gmail.com>
2024-01-15 14:28:15 +0000

    CFB6.2

---
+++ aula6.2/Program.cs
@@ -0,0 +1,14 @@
+﻿namespace aula6._2
+{
+    internal class Program
+    {
+        static void Main(){
+
+            int n1, n2, n3;
+
+            n1 = 10; n2 = 20; n3 = 30;
+
+            Console.Write("\nn1={0}\nn2={1}\nn3={2}\n",n1,n2,n3);
+        }
+    }
+}
